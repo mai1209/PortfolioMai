05d860c4c574c6e5fdd0059f1f9e3cf2d54f7fe0
Mai <maidev@Mac.fibertel.com.ar>
2025-07-01 23:20:56 -0300

    hashRouter

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
+  import './index.css';
+  import App from './components/App.jsx';
+  import React from 'react';
+  import ReactDOM from 'react-dom/client';
+  import Contact from './components/Contact.jsx';
+  import Work from './components/Work.jsx';
+  // 1. CAMBIA LA IMPORTACIÓN AQUÍ
+  import { HashRouter, Routes, Route } from 'react-router-dom'; 
+  import reportWebVitals from './reportWebVitals.js';
 
-import './index.css';
-import App from './components/App.jsx';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Contact from './components/Contact.jsx';
-import Work from './components/Work.jsx';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
-import reportWebVitals from './reportWebVitals.js';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/work" element={<Work />} />
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      {/* 2. Y CAMBIA EL COMPONENTE AQUÍ */}
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/work" element={<Work />} />
         <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+        </Routes>
+      </HashRouter>
+    </React.StrictMode>
+  );
 
-reportWebVitals();
+  reportWebVitals();
